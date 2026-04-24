d3befe52eb9c8143b82e1c4d4a2804a1b8f32e4d
yyxi <178086254+yyxi@users.noreply.github.com>
2026-04-24 14:00:28 +0000

    feat: initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { configDefaults, defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      exclude: [
+        '**/example*.ts',
+        'src/test-support/**',
+        'src/vendor/**',
+        ...(configDefaults.coverage.exclude ?? []),
+      ],
+      include: ['src/**'],
+      provider: 'v8',
+    },
+    include: [],
+    passWithNoTests: true,
+    projects: ['vitest.config.*.ts'],
+  },
+})
